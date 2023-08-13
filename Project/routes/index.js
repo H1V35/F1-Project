c675ae81dc3562bd8c073bcfaca7c32b920813e2
albertocomb <combattialberto@gmail.com>
2023-08-13 18:49:50 +0200

    Adding routes

--- Project/routes/index.js
+++ Project/routes/index.js
@@ -2,8 +2,19 @@ module.exports = app => {
   const indexRoutes = require('./index.routes')
   app.use('/', indexRoutes)
 
-  //   const placesRoutes = require('./places.routes')
-  //   app.use('/places', placesRoutes)
+  const racesRoutes = require('./races.routes')
+  app.use('/races', racesRoutes)
+
+  const standingRoutes = require('./standings.routes')
+  app.use('/standing', standingRoutes)
+
+  const driverRoutes = require('./driver.routes')
+  app.use('/driver', driverRoutes)
+
+  const teamRoutes = require('./team.routes')
+  app.use('/team', teamRoutes)
+
+
 
   //   const apiRoutes = require('./api.routes')
   //   app.use('/api', apiRoutes)
