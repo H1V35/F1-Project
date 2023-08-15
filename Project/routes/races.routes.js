90414e651ebec0124baa65c6ba7f2c0e868fc9de
albertocomb <combattialberto@gmail.com>
2023-08-15 18:22:46 +0200

    Schedule

--- Project/routes/races.routes.js
+++ Project/routes/races.routes.js
@@ -4,17 +4,13 @@ const f1Api = require('../services/f1.service')
 router.get('/schedule', (req, res, next) => {
     f1Api
         .getAllRaces()
-        .then(responseFromAPI => {
-            res.render('races/schedule', { races: responseFromAPI.data.MRData.RaceTable.Races })
-        })
-        .catch(err => console.log(err))
-})
-
-router.get('/results', (req, res, next) => {
-    f1Api
-        .getAllResults()
-        .then(responseFromAPI => {
-            res.send(responseFromAPI.data)
+        .then(newRacesFromAPI => {
+            const allRaces = newRacesFromAPI.data.MRData.RaceTable.Races;
+            const currentDate = new Date();
+            const newRaces = allRaces.filter(race => new Date(race.date) > currentDate);
+            const oldRaces = allRaces.filter(race => new Date(race.date) < currentDate).reverse();
+            const locations = allRaces.map(race => race.Circuit.Location);
+            res.render('races/schedule', { new_races: newRaces, old_races: oldRaces, locations: locations })
         })
         .catch(err => console.log(err))
 })
